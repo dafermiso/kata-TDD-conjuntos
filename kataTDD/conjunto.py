4d10cf257c1ba16cb1973c667a8a52977be3aa29
dafermiso <df.guerrerov1@uniandes.edu.co>
2024-02-18 16:11:10 -0500

    Caso 4: Calcular promedio con un conjunto de n elementos (Azul)

--- kataTDD/conjunto.py
+++ kataTDD/conjunto.py
@@ -2,12 +2,19 @@ class Conjunto:
   def __init__(self, conjunto):
     self.__conjunto = conjunto
 
+  # def promedio(self):
+  #   if len(self.__conjunto) == 1:
+  #     return (self.__conjunto[0])
+  #   elif len(self.__conjunto) == 2:
+  #     return (self.__conjunto[0] + self.__conjunto[1]) / 2
+  #   elif len(self.__conjunto) > 2:
+  #     return sum(self.__conjunto) / len(self.__conjunto)
+  #   else:
+  #     return None
+
+  # REFACTORY:
   def promedio(self):
-    if len(self.__conjunto) == 1:
-      return (self.__conjunto[0])
-    elif len(self.__conjunto) == 2:
-      return (self.__conjunto[0] + self.__conjunto[1]) / 2
-    elif len(self.__conjunto) > 2:
+    if len(self.__conjunto) > 0:
       return sum(self.__conjunto) / len(self.__conjunto)
     else:
       return None
